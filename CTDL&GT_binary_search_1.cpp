e60dbb80b071bef4f0de00a1f0db45d4e8e5da73
Trần công Đạt <125424696+trancongdat159@users.noreply.github.com>
2023-04-05 13:02:49 +0700

    Create CTDL&GT_binary_search_1.cpp

---
+++ CTDL&GT_binary_search_1.cpp
@@ -0,0 +1,32 @@
+int BinarySearch(int a[],int n,int x)
+{
+	int left, right, mid; left=0; right=n-1;
+	do{
+		mid=(left+right)/2;
+		if(a[mid]==x){
+			return mid;
+		}
+		else if(a[mid]<x){
+			left=mid+1;
+		}
+		else{
+			right=mid-1;
+		}
+	}while(left<=right);
+	return -1;
+}
+
+// Dùng đệ quy 
+int BinarySearch_Recursive(int a[],int x,int left,int right){
+	if(left>right){
+		return -1;
+	}
+	int mid=(left+right)/2;
+	if(x==a[mid]){
+		return mid;
+	}
+	if(x<a[mid]){
+		return BinarySearch_Recursive(a,x,left,mid-1);
+	}
+	return BinarySearch_Recursive(a,x,mid+1,right);
+}
