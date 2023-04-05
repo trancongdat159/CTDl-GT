518b45b4b5f9a3fe258773503047c50381981bd7
Trần công Đạt <125424696+trancongdat159@users.noreply.github.com>
2023-04-05 13:43:06 +0700

    Create CTDL&GT_Quick_Sort_1.cpp

---
+++ CTDL&GT_Quick_Sort_1.cpp
@@ -0,0 +1,38 @@
+//Hàm partition()
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];    // khai báo phần tử đánh dâu pivot
+    int left = low;   //khai báo biến bên trái
+    int right = high - 1; //khai báo biến bên phải
+    while(true){
+        while(left <= right && arr[left] < pivot) left++; // tìm phần tử >= phần tử pivot trong mảng
+        while(right >= left && arr[right] > pivot) right--; // tìm phần tử <= phần tử pivot trong mảng
+        if (left >= right) break; // sau khi duyệt xong thì thoát khỏi vòng lặp
+        swap(arr[left], arr[right]); // nếu chưa xong thì sử dụng hàm swap() để tráo đổi.
+        left++; // Vì left hiện tại đã xét, nên cần tăng
+        right--; // Vì right hiện tại đã xét, nên cần giảm
+    }
+    swap(arr[left], arr[high]);
+    return left; // Trả về chỉ số sẽ dùng để chia đôi mảng
+}
+//Hàm quicksort()
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* index là chỉ số nơi phần tử này đã đứng đúng vị trí
+         và đây là phần tử chia mảng làm 2 mảng con trái & phải */
+        int index = partition(arr, low, high);
+  
+        // Gọi đệ quy sắp xếp 2 mảng con trái và phải
+        quickSort(arr, low, index - 1);
+        quickSort(arr, index + 1, high);
+    }
+}
+//Hàm swap()
+void swap(int &a, int &b)
+{
+    int t = a;
+    a = b;
+    b = t;
+}
